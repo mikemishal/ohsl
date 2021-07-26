1dc577b07ad6ca40537187c6222ce678820869a4
Michael Mishal <mimishal@microsoft.com>
2021-07-26 12:02:15 -0500

    First Function

---
+++ ohslproductid.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ohsl.Function
+{
+    public static class ohslproductid
+    {
+        [FunctionName("ohslproductid")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string productId = req.Query["productId"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            productId = productId ?? data?.productId;
+
+            string responseMessage = string.IsNullOrEmpty(productId)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"The product name for your product id {productId} is Starfruit Explosion";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
